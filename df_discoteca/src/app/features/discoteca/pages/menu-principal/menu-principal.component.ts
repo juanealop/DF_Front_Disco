7580170a5863a493278a9652aefedf671780c893
juanealop <88118954+juanealop@users.noreply.github.com>
2026-09-06 14:47:00 -0500

    se agrego autenticacion de discoteca

---
+++ df_discoteca/src/app/features/discoteca/pages/menu-principal/menu-principal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-principal',
+  imports: [],
+  templateUrl: './menu-principal.component.html',
+  styleUrl: './menu-principal.component.scss'
+})
+export class MenuPrincipalComponent {
+
+}
